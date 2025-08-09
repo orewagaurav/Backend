3cb144ca9e343e76572c5e3f9ab2771a7ffe34ee
orewagaurav <gauravkumar47967@gmail.com>
2025-08-09 12:31:43 +0530

    feat: add CORS and cookie-parser middleware; implement database connection logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from 'dotenv'
-import connectDB from "./db/datbase.js";
-
-connectDB();
-
+import app from './app.js'
+import connectDB from "./db/database.js";
 dotenv.config({
-    path:"./env"
+    path:"./.env"
 })
+
+connectDB()
+.then(()=>{
+    app.on("error",(e)=>{
+        console.log("Error: ",e);
+        throw e
+        
+    })
+    app.listen(process.env.PORT || 3000),()=>{
+        console.log(`server is runing at port: ${process.env.PORT || 3000}`)
+    }
+})
+.catch((e)=>{
+    console.log("MongoDB connection error: ",e);
+    
+})
+
+
