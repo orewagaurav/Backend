95480e2bf8142907ca83b27b1e19a11415cff071
orewagaurav <gauravkumar47967@gmail.com>
2025-08-13 00:40:42 +0530

    feat: add asyncHandler utility for handling asynchronous route handlers

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials:true
 }))
-
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 export default app;
