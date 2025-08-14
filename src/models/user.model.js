d9674402b9958310e447457c79d7de57f5291cce
orewagaurav <gauravkumar47967@gmail.com>
2025-08-15 00:02:27 +0530

    feat: update user model with password hashing and token generation; modify .env.sample for configuration

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,67 +1,91 @@
-import mongoose ,{Schema} from 'mongoose' 
-import jst from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import mongoose, { Schema } from "mongoose";
+import jst from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
-    {
-        username:{
-            type:String,
-            required:true,
-            unique:true,
-            lowercase:true,
-            trim:true,
-            index:true
-        },
-        email:{
-            type:String,
-            required:true,
-            unique:true,
-            lowercase:true,
-            trim:true
-        },
-        fullName:{
-            type:String,
-            required:true,
-            trim:true,
-            index:true
-        },
-        avtar:{
-            type:String, //cloudinary url
-            required:true,
-        },
-        coverImage:{
-            type:String, //cloudinary url
-
-        },
-        watchHistory:[
-            {
-            type: Schema.Types.ObjectId,
-            ref:"Video"
-            }
-        ],
-        password:{
-            type:String,
-            required:[true,"password is reqired"]
-        },
-        refreshToken:{
-            type:String
-        }
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avtar: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is reqired"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return bcrypt.compare("password", this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
     {
-        timestamps:true
+      _id: this.id,
+      email: this.email,
+      fullName: this.fullName,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-)
-
-userSchema.pre("save",async function (next){
-    if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password,10)
-    next()
-    }
-)
-
-userSchema.methods.isPasswordCorrect = async function(password){
-    return bcrypt.compare("password",this.password)
+  )
 }
-
-export const User = mongoose.model("User",userSchema)
+userSchema.methods.generateRefreshToken = function () {
+     return jwt.sign(
+    {
+      _id: this.id,
+    },
+    process.env.REFERESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFERESH_TOKEN_EXPIRY,
+    }
+  )
   
+};
+
+export const User = mongoose.model("User", userSchema);
