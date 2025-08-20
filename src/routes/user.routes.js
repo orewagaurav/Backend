add77bae9e0fb3617bd9bef98b816a9474f21cc1
orewagaurav <gauravkumar47967@gmail.com>
2025-08-21 01:38:04 +0530

    fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,7 +13,7 @@ router.route("/register").post(
             maxCount:1
         },
         {
-            name:coverImage,
+            name:"coverImage",
             maxCount:1
         }
     ]),
